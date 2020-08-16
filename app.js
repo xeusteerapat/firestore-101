8535b66ed89ad908e6b42e33fe2c3e1ea5a26baf
xeusteerapat <xeus085@gmail.com>
2020-08-16 16:32:30 +0700

    retrieve data from firestore and render

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log(db);
+const cafeList = document.querySelector('#cafe-list');
+
+// create element and render
+function renderCafe(doc) {
+  let listCafe = document.createElement('li');
+  let name = document.createElement('span');
+  let city = document.createElement('span');
+
+  listCafe.setAttribute('data-id', doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+
+  listCafe.appendChild(name);
+  listCafe.appendChild(city);
+
+  cafeList.appendChild(listCafe);
+}
+
+db.collection('cafes')
+  .get()
+  .then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      renderCafe(doc);
+    });
+  });
